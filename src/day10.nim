74ca0a6c950250d2de2ebbd21de84e509167d62d
Paulius Imbrasas <paulius@prehendo.io>
2023-12-13 22:07:41 +0000

    Implement day 10 part 2 in the worst way possible

--- src/day10.nim
+++ src/day10.nim
@@ -10,6 +10,9 @@ import tables
 import sugar
 import sets
 import unicode
+import arraymancer
+
+import strides
 
 func convertToUtf8(s: char): string =
   case s:
@@ -20,6 +23,13 @@ func convertToUtf8(s: char): string =
     of 'L': Rune(0x2514).toUTF8
     of 'J': Rune(0x2518).toUTF8
     else: @[s].toString
+
+proc prettyPrintTensor(t: Tensor[char]) =
+  for y in 0..<t.shape[0]:
+    for x in 0..<t.shape[1]:
+      stdout.write(t[y, x].convertToUtf8)
+    echo ""
+  echo ""
 
 type Coord = tuple
   y: int
@@ -38,6 +48,27 @@ func findStart(matrix: seq[string], maxY, maxX: int): Coord =
     for x in 1..<maxX:
       if matrix[y][x] == 'S':
         return (y, x)
+
+func contractSubGrid(t: Tensor[char]): char =
+  if t[1, 0] == '#' and t[2, 1] == '#': '7'
+  elif t[1, 0] == '#' and t[0, 1] == '#': 'J'
+  elif t[1, 0] == '#' and t[1, 2] == '#': 'L'
+  elif t[1, 2] == '#' and t[2, 1] == '#': 'F'
+  elif t[1, 0] == '#': '-'
+  elif t[1, 1] == '#': '|'
+  elif t[1, 1] == 'O': 'O'
+  elif t[1, 1] == 'I': 'I'
+  else: '.'
+
+proc contractMatrix(t: Tensor[char]): Tensor[char] =
+  var grid: seq[seq[char]]
+  for y in 0..<t.shape[0] @: 3:
+    var line: seq[char]
+    for x in 0..<t.shape[1] @: 3:
+      let ch = contractSubGrid(t[y..(y+2), x..(x+2)])
+      line.add(ch)
+    grid.add(line)
+  grid.toTensor
 
 func part1(): int =
   let matrix = block:
@@ -92,16 +123,22 @@ func part1(): int =
 
 proc part2(): int =
   var matrix = block:
-    var inp = input.example5.splitLines.mapIt(fmt".{it}.")
-    inp[0] = ".".repeat(inp[1].len)
-    inp[^1] = ".".repeat(inp[1].len)
-    inp
+    var inp = input.input.splitLines.mapIt(fmt".{it}.".toSeq)
+    inp[0] = ".".repeat(inp[1].len).toSeq
+    inp[^1] = ".".repeat(inp[1].len).toSeq
+    inp.toTensor
 
-  let maxY = matrix.len
-  let maxX = matrix[0].len
+  let maxY = matrix.shape[0]
+  let maxX = matrix.shape[1]
 
   # find starting point
-  let startCoord = findStart(matrix, maxY, maxX)
+  let startCoord = block:
+    var l: Coord
+    for loc, c in matrix:
+      if c == 'S':
+        l = (loc[0], loc[1])
+        break
+    l
   var
     distanceMap = { startCoord: 0 }.toTable
     queue: Deque[(Coord, int)] = @[(startCoord, 0)].toDeque
@@ -115,7 +152,7 @@ proc part2(): int =
       s = south(v)
 
     # check possible moves
-    let possibleMoves = case matrix.at(v):
+    let possibleMoves = case matrix[v.y, v.x]:
       of '|': @[n, s]
       of '-': @[e, w]
       of 'L': @[n, e]
@@ -124,40 +161,102 @@ proc part2(): int =
       of 'F': @[s, e]
       of 'S':
         var outs: seq[Coord]
-        if matrix.at(n) == '|' or matrix.at(n) == 'F' or matrix.at(s) == '7':
+        if matrix[n.y, n.x] == '|' or matrix[n.y, n.x] == 'F' or matrix[n.y, n.x] == '7':
           outs.add(n)
-        if matrix.at(s) == '|' or matrix.at(s) == 'J' or matrix.at(s) == 'L':
+        if matrix[s.y, s.x] == '|' or matrix[s.y, s.x] == 'J' or matrix[s.y, s.x] == 'L':
           outs.add(s)
-        if matrix.at(e) == '-' or matrix.at(e) == 'J' or matrix.at(e) == '7':
+        if matrix[e.y, e.x] == '-' or matrix[e.y, e.x] == 'J' or matrix[e.y, e.x] == '7':
           outs.add(e)
-        if matrix.at(w) == '-' or matrix.at(w) == 'L' or matrix.at(w) == 'F':
+        if matrix[w.y, w.x] == '-' or matrix[w.y, w.x] == 'L' or matrix[w.y, w.x] == 'F':
           outs.add(w)
         outs
       else: @[]
 
-    # for every move
-    #  check move in distanceMap, if it's
     for mv in possibleMoves:
       if not distanceMap.hasKeyOrPut(mv, dist + 1):
         queue.addLast((mv, dist + 1))
 
-  proc printMatrix(m: seq[string]) =
-    for y in 0..<m.len:
-      echo m[y]
+  for coord, c in matrix:
+    let d = distanceMap.getOrDefault((coord[0], coord[1]), -1)
+    if d == -1:
+      matrix[coord[0], coord[1]] = '.'
 
+  let NS = @[
+      @['.', '#', '.'],
+      @['.', '#', '.'],
+      @['.', '#', '.'],
+    ].toTensor
 
+  let EW = @[
+      @['.', '.', '.'],
+      @['#', '#', '#'],
+      @['.', '.', '.'],
+    ].toTensor
+
+  let NW = @[
+      @['.', '#', '.'],
+      @['.', '#', '#'],
+      @['.', '.', '.'],
+    ].toTensor
+
+  let NE = @[
+      @['.', '#', '.'],
+      @['#', '#', '.'],
+      @['.', '.', '.'],
+    ].toTensor
+
+  let SW = @[
+      @['.', '.', '.'],
+      @['#', '#', '.'],
+      @['.', '#', '.'],
+    ].toTensor
+
+  let SE = @[
+      @['.', '.', '.'],
+      @['.', '#', '#'],
+      @['.', '#', '.'],
+    ].toTensor
+
+  let dot = @[
+      @['.', '.', '.'],
+      @['.', '.', '.'],
+      @['.', '.', '.'],
+    ].toTensor
+
+  let S = @[
+      @['.', '#', '.'],
+      @['.', '#', '#'],
+      @['.', '.', '.'],
+    ].toTensor
+
+  let expansionMap = {
+    '|': NS, #
+    '-': EW, #
+    'L': NW, #
+    'J': NE, #
+    '7': SW, #
+    'F': SE, #
+    'S': S,
+    '.': dot, #
+  }.toTable
+
+  var expandedMatrix: seq[Tensor[char]]
   for y in 0..<maxY:
-    for x in 0..<maxX:
-      let d = distanceMap.getOrDefault((y, x), -1)
-      if d == -1:
-        matrix[y][x] = '.'
+    var expandedLine: Tensor[char] = expansionMap[matrix[y, 0]]
+    for x in 1..<maxX:
+      let output = expansionMap[matrix[y, x]]
+      expandedLine = concat(expandedLine, output, axis = 1)
+    expandedMatrix.add(expandedLine)
+
+  matrix = expandedMatrix.foldl(concat(a, b, axis = 0))
+  let exMaxY = matrix.shape[0]
+  let exMaxX = matrix.shape[1]
 
   # bfs all dots
-  let outerDotLocations = block:
+  let outerDots = block:
     var
       queue = @[(0, 0).Coord].toDeque
       visited = @[(0, 0).Coord].toHashSet
-      maxY = matrix.len
 
     while queue.len() > 0:
       let v = queue.popFirst
@@ -165,7 +264,7 @@ proc part2(): int =
       let possibleMoves = collect:
         for y in v.y-1..v.y+1:
           for x in v.x-1..v.x+1:
-            if y > -1 and y < maxY and x > -1 and x < maxX and matrix[y][x] == '.':
+            if y > -1 and y < exMaxY and x > -1 and x < exMaxX and matrix[y, x] == '.':
               (y, x).Coord
 
       for mv in possibleMoves:
@@ -173,77 +272,19 @@ proc part2(): int =
           queue.addLast((mv.y, mv.x))
     visited
 
-  for idx in outerDotLocations:
-    matrix[idx.y][idx.x] = ' '
+  let innerDots = collect:
+    for coord, c in matrix:
+      let loc = (coord[0], coord[1]).Coord
+      if not outerDots.contains(loc) and matrix[loc.y, loc.x] == '.':
+         { loc }
 
-  # printMatrix(matrix)
+  for dot in innerDots:
+    matrix[dot.y, dot.x] = 'I'
 
-  # const symbols = "|-LJ7FS".toHashSet
-  const symbols = "|-LJ7FS".toHashSet
+  for dot in outerDots:
+    matrix[dot.y, dot.x] = 'O'
 
-  var dots: int
-  for y in 0..<maxY:
-    var inside = false
-    var prevSymbol: char = '.'
-    var crosses: int
-    for x in 0..<maxX:
-      var thisSymbol = matrix[y][x]
-
-      # if symbols.contains(prevSymbol) and thisSymbol == '.':
-      #   inside = not inside
-      #   if inside:
-      #     matrix[y][x] = 'o'
-      #     inc(dots)
-      # elif prevSymbol == '.' and symbols.contains(thisSymbol):
-      #   inside = not inside
-
-      let isNotCross =
-        (prevSymbol == 'L' and thisSymbol == '-') or
-        (prevSymbol == 'F' and thisSymbol == '-') or
-        (prevSymbol == '-' and thisSymbol == '-')
-
-
-      if symbols.contains(thisSymbol):
-        # let crossPolygon =
-        #   (prevSymbol == 'L' and thisSymbol != '-') or
-        #   (prevSymbol == 'F' and thisSymbol != '-') or
-        #   (prevSymbol == '-' and thisSymbol != 'J') or
-        #   (prevSymbol == '-' and thisSymbol != '7')
-
-
-
-        if isNotCross:
-          discard
-        else: # is cross
-          inc(crosses)
-
-      elif matrix[y][x] == '.' and crosses.mod(2) != 0:
-        matrix[y][x] = 'I'
-        inc(dots)
-      elif matrix[y][x] == '.' and crosses.mod(2) == 0:
-        matrix[y][x] = 'O'
-
-      if y == 5:
-        echo fmt"{thisSymbol.convertToUtf8} <- inside:{inside}, insideViaCrosses={crosses.mod(2) == 0}, isCross={not isNotCross}"
-
-      prevSymbol = matrix[y][x]
-
-  # echo dots
-
-  let newMatrix = block:
-    var newMatrix: seq[string]
-    for y in 0..<maxY:
-
-      var thisLine: string
-      for x in 0..<maxX:
-        thisLine.add(matrix[y][x].convertToUtf8)
-
-      newMatrix.add(thisLine)
-    newMatrix
-
-  printMatrix(newMatrix)
-
-  dots
+  contractMatrix(matrix).countIt(it == 'I')
 
 block:
   let start = cpuTime()
